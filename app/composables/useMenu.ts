5b97d5b83ff3511cb5b42feb7361937a454789c1
Diego Álvarez <diegoalfonso.aa@gmail.com>
2026-07-21 15:32:46 -0400

    worked on app composables on menu admin page

--- app/composables/useMenu.ts
+++ app/composables/useMenu.ts
@@ -5,3 +5,50 @@ export function useMenu() {
   return data
 
 }
+
+export function useAdminMenu(){
+  const data = useFetch('/api/admin/menu/categories')
+  return data
+}
+
+// CRUD Categorías
+export async function createAdminCategory(data: Record<string, unknown>) {
+  return await $fetch('/api/admin/menu/categories', {
+    method: 'POST',
+    body: data,
+  })
+}
+
+export async function updateAdminCategory(id: number, data: Record<string, unknown>) {
+  return await $fetch(`/api/admin/menu/categories/${id}`, {
+    method: 'PUT',
+    body: data,
+  })
+}
+
+export async function deleteAdminCategory(id: number) {
+  return await $fetch(`/api/admin/menu/categories/${id}`, {
+    method: 'DELETE',
+  })
+}
+
+// CRUD Productos
+export async function createAdminProduct(data: Record<string, unknown>) {
+  return await $fetch('/api/admin/menu/products', {
+    method: 'POST',
+    body: data,
+  })
+}
+
+export async function updateAdminProduct(id: number, data: Record<string, unknown>) {
+  return await $fetch(`/api/admin/menu/products/${id}`, {
+    method: 'PUT',
+    body: data,
+  })
+}
+
+export async function deleteAdminProduct(id: number) {
+  return await $fetch(`/api/admin/menu/products/${id}`, {
+    method: 'DELETE',
+  })
+}
