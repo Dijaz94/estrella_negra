ea19391810c65f44591edb98d9d23d3da5ec3732
Diego Álvarez <diegoalfonso.aa@gmail.com>
2026-07-31 22:28:28 -0400

    finished endpoints for users page

--- server/api/admin/usuarios/[id].put.ts
+++ server/api/admin/usuarios/[id].put.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import  z from 'zod'
  const schema = z.object({
     nombre: z.string().min(1).max(50).optional(),
     apellido: z.string().min(1).max(50).optional(),
@@ -13,18 +13,30 @@ export default defineEventHandler(async (event) => {
   if (!id) {
     throw createError({ statusCode: 400, statusMessage: 'ID inválido' })
   }
+  
 
  
 
   const body = await readBody(event)
   const parsed = schema.safeParse(body)
 
-  if (!parsed.success) {
+  const currentUser = event.context.user as { id_usuario: number }
+
+    if (!parsed.success) {
     throw createError({
       statusCode: 400,
       message: parsed.error.issues.map(i => `${i.path.join('.')}: ${i.message}`).join(', '),
     })
   }
+
+
+  if (currentUser.id_usuario === id && parsed.data?.activo === false){
+    throw createError({ statusCode: 400, statusMessage: 'No puedes desactivar tu propia cuenta' })
+  }
+  if (currentUser.id_usuario === id && parsed.data?.rol === 'EDITOR'){
+    throw createError({ statusCode: 400, statusMessage: 'No puedes cambiar tu propio rol' })
+  }
+
 
   const updated = await updateUser(id, parsed.data)
   return updated
